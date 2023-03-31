d1c53e22036dcd4f082abf9b00303f78fd8ac12d
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-31 15:05:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script>
+import axios from 'axios';
+
+//ARRAY FILMS///
+import { store } from './store.js';
+
+import TheHeader from './components/TheHeader.vue';
+
+export default {
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    //ARRAY FILMS///
+    getMovies() {
+
+      let urlApi = "https://api.themoviedb.org/3/search/movie?api_key=6d55564c6ea75d27119d87e52f2f5d40";
+
+      if (store.movies.length > 0) {
+        urlApi += `&query=${store.input}`;
+        console.log(store.input);
+        console.log(urlApi);
+      }
+
+      axios.get(urlApi)
+        .then(response => {
+
+          this.store.movies = response.data.results;
+          console.log(this.store.movies)
+        })
+
+
+    },
+
+  },
+  created() {
+    //ARRAY FILMS///
+    this.getMovies();
+
+  },
+  computed: {
+  },
+  components: {
+    TheHeader
+  }
+}
+
+</script>
+
+<template>
+  <TheHeader />
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi quia quas assumenda quasi accusamus optio hic,
+        recusandae ratione facilis iusto.</p>
+</template>
+
+<script>
+export default {
+    name: 'TheHeader'
+}
+</script>
+
+<style lang="scss"></style>
