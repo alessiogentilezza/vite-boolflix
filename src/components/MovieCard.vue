e1820d6a8530be9131b63027ec127e2404a43724
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-31 17:48:54 +0200

    caricate bandiere in assets

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -2,21 +2,38 @@
     <div class="wrap">
         <h3>titolo film {{ titolo }}</h3>
         <h3>titolo originale {{ titoloOriginale }}</h3>
-        <h3>lingua {{ lingua }}</h3>
+        <div>
+            <h3>lingua {{ lingua }} </h3>
+            {{ bandiera() }}
+        </div>
         <h3>voto {{ voto }}</h3>
+        <!-- <img src="../assets/it.png"> -->
+
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'MovieCard',
-        props: {
-            titolo: String,
-            titoloOriginale: String,
-            lingua: String,
-            voto: Number,
-        }
+export default {
+    name: 'MovieCard',
+    props: {
+        titolo: String,
+        titoloOriginale: String,
+        lingua: String,
+        voto: Number,
+    },
+    methods: {
+        bandiera() {
+            let bandiera = "";
+
+            if (this.lingua == "it") {
+                bandiera = "../assets/it.png"
+            } else {
+
+            }
+            return bandiera
+        },
     }
+}
 </script>
 
 
@@ -24,6 +41,9 @@
 .wrap {
     border-top: 1px red solid;
     margin: 1px;
+}
 
+img {
+    height: 20px;
 }
 </style>
